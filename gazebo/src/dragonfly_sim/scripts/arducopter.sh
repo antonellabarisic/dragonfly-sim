3e4275402bb07e7b3e598093ca3539bdaa9fe2de
antonella.barisic <antonella.barisic@fer.hr>
2022-04-12 09:50:06 -0600

    Increase rate of mavros topics to 50Hz

--- gazebo/src/dragonfly_sim/scripts/arducopter.sh
+++ gazebo/src/dragonfly_sim/scripts/arducopter.sh
@@ -13,4 +13,4 @@ fi
 
 cd /workspace
 mkdir -p temp/$temp_directory
-./ardupilot/Tools/autotest/sim_vehicle.py -L $location -v ArduCopter -f gazebo-iris -I $instance --add-param-file $param_file --use-dir temp/$temp_directory
+./ardupilot/Tools/autotest/sim_vehicle.py -L $location -v ArduCopter -f gazebo-iris -I $instance --add-param-file $param_file --use-dir temp/$temp_directory -m --streamrate=50
